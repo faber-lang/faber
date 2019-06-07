ff58979d4268b62598dc4bc82629a8eb36a2e35b
coord.e <me@coord-e.com>
2019-06-07 08:32:20 +0000

    Fix: Fix more mistakes in Typing

--- src/Typing.hs
+++ src/Typing.hs
@@ -3,6 +3,8 @@ module Typing where
 import qualified Nameless as N
 import qualified Data.Map as Map
 import qualified Data.Set as Set
+import Data.Maybe (fromJust)
+import Data.List (elemIndex)
 import Control.Monad.State
 import Control.Monad.Except
 
@@ -17,12 +19,15 @@ newtype TypeEnv = TypeEnv [Type]
 
 initEnv :: TypeEnv
 initEnv = TypeEnv []
-lookup :: TypeEnv -> TVar -> Type
-lookup (TypeEnv env) = fromJust . elemIndex env
-append :: TypeEnv -> Int -> TypeEnv
-append (TypeEnv env) = TypeEnv . (: env)
+lookupEnv :: TypeEnv -> TVar -> Type
+lookupEnv (TypeEnv env) = fromJust . elemIndex env
+appendEnv :: TypeEnv -> Int -> TypeEnv
+appendEnv (TypeEnv env) = TypeEnv . (: env)
 
 type Subst = Map.Map TVar Type
+
+nullSubst :: Subst
+nullSubst = Map.empty
 
 class Substitutable a where
   apply :: Subst -> a -> a
@@ -40,7 +45,7 @@ instance Substitutable Type where
 instance Substitutable TypeEnv where
   apply s (TypeEnv env) = TypeEnv $ map (apply s) env
 
-  ftv (TypeEnv env) = foldr (Set.union . ftv) Set.empty
+  ftv (TypeEnv env) = foldr (Set.union . ftv) Set.empty env
 
 compose :: Subst -> Subst -> Subst
 s1 `compose` s2 = Map.map (apply s1) s2 `Map.union` s1
@@ -59,7 +64,7 @@ data TypeError
 runInfer :: Infer (Subst, Type) -> Either TypeError Type
 runInfer m = case evalState (runExceptT m) initUnique of
   Left err  -> Left err
-  Right res -> Right $ closeOver res
+  Right (_, t) -> Right t
 
 fresh :: Infer Type
 fresh = do
@@ -70,7 +75,7 @@ fresh = do
 unify :: Type -> Type -> Infer Subst
 unify (Function a1 b1) (Function a2 b2) = do
   s_a <- unify a1 a2
-  s_b <- unify (apply s_a b1) (apply s_b b2)
+  s_b <- unify (apply s_a b1) (apply s_a b2)
   return $ s_a `compose` s_b
 unify (Variable i) t = bind i t
 unify t (Variable i) = bind i t
@@ -79,24 +84,24 @@ unify t1 t2 = throwError $ UnificationFail t1 t2
 
 bind :: Int -> Type -> Infer Subst
 bind i t | t == Variable i = return nullSubst
-         | occursCheck i t = throwError $ InifiniteType i t
+         | occursCheck i t = throwError $ InfiniteType i t
          | otherwise       = return $ Map.singleton i t
 
 occursCheck :: Substitutable a => Int -> a -> Bool
-occursCheck i t = a `Set.member` ftv t
+occursCheck i t = i `Set.member` ftv t
 
 type Preset = Map.Map String Type
 
 infer :: Preset -> TypeEnv -> N.Expr -> Infer (Subst, Type)
 infer p env e = case e of
-  N.Bound i -> return (nullSubst, lookup env i)
+  N.Bound i -> return (nullSubst, lookupEnv env i)
   N.Free s -> case Map.lookup s p of
     Nothing -> throwError $ UnboundVariable s
     Just t -> return (nullSubst, t)
   N.Integer _ -> return (nullSubst, Integer)
   N.Lambda body -> do
     tv <- fresh
-    (s, ret) <- infer p (append env tv) body
+    (s, ret) <- infer p (appendEnv env tv) body
     return (s, Function (apply s tv) ret)
   N.Apply a b -> do
     tv <- fresh
