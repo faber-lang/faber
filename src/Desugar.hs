56e4cbbf00da179e3dbf570a05eb5e8980e382bb
coord.e <me@coord-e.com>
2019-06-07 10:27:35 +0000

    Add: Implement some conversion on arithmetic operators

--- src/Desugar.hs
+++ src/Desugar.hs
@@ -1,12 +1,15 @@
 module Desugar where
 
 import qualified Parse as P
+import qualified Operators as Op
 
 data Expr
   = Integer Int
   | Lambda String Expr
   | Apply Expr Expr
   | Variable String
+  | BinaryOp Op.BinaryOp Expr Expr
+  | SingleOp Op.SingleOp Expr
   deriving (Show)
 
 desugar_lambda :: [String] -> Expr -> Expr
@@ -18,3 +21,5 @@ desugar (P.Lambda ps body) = desugar_lambda ps $ desugar body
 desugar (P.Integer i) = Integer i
 desugar (P.Apply a b) = Apply (desugar a) (desugar b)
 desugar (P.Variable x) = Variable x
+desugar (P.BinaryOp op a b) = BinaryOp op (desugar a) (desugar b)
+desugar (P.SingleOp op x) = SingleOp op $ desugar x
--- src/Nameless.hs
+++ src/Nameless.hs
@@ -3,6 +3,7 @@ module Nameless where
 import Data.List
 import Data.Maybe
 import qualified Desugar as D
+import qualified Operators as Op
 
 data Expr
   = Integer Int
@@ -10,6 +11,8 @@ data Expr
   | Apply Expr Expr
   | Free String
   | Bound Int
+  | BinaryOp Op.BinaryOp Expr Expr
+  | SingleOp Op.SingleOp Expr
   deriving (Show)
 
 nameless' :: [String] -> D.Expr -> Expr
@@ -17,6 +20,8 @@ nameless' t (D.Apply fn arg) = Apply (nameless' t fn) (nameless' t arg)
 nameless' t (D.Lambda p body) = Lambda $ nameless' (p : t) body
 nameless' t (D.Variable name) = maybe (Free name) Bound $ elemIndex name t
 nameless' _ (D.Integer i) = Integer i
+nameless' t (D.BinaryOp op a b) = BinaryOp op (nameless' t a) (nameless' t b)
+nameless' t (D.SingleOp op x) = SingleOp op $ nameless' t x
 
 nameless :: D.Expr -> Expr
 nameless = nameless' []
