55ee276d456dc4893584632ab34af798d5c65f34
coord.e <me@coord-e.com>
2019-06-10 09:00:31 +0000

    Add: Add test cases of Hoist module

---
+++ test/HoistSpec.hs
@@ -0,0 +1,26 @@
+module HoistSpec (spec) where
+
+import Test.Hspec
+import qualified Closure as C
+import Hoist
+
+spec :: Spec
+spec = do
+  describe "function hoisting" $ do
+    it "hoist functions" $ do
+      hoist (C.Function C.Parameter) `shouldBe` Module [Function (Parameter 1)] (FunctionRef 0)
+      hoist (C.Function $ C.Function $ C.Parameter) `shouldBe` Module [Function (Parameter 1), Function (FunctionRef 0)] (FunctionRef 1)
+
+    it "convert function calls" $ do
+      hoist (C.Apply (
+              C.Tuple [
+                C.Function C.Parameter,
+                C.Tuple []
+              ])
+              (C.Integer 1)
+            ) `shouldBe` Module [Function $ Parameter 1] (
+              LocalLet (Tuple [FunctionRef 0, Tuple []])
+              (
+                Call (NthOf 0 LetBound) [NthOf 1 LetBound, Integer 1]
+              )
+            )
