d9a9ecb4412a661ae80d1802c65b031c996e4a46
coord.e <me@coord-e.com>
2019-06-13 06:35:06 +0000

    Add: Roughtly implement lazy -> strict conversion

---
+++ src/Lazy.hs
@@ -0,0 +1,56 @@
+module Lazy where
+
+import qualified Nameless as N
+import qualified Operators as Op
+
+data Expr
+  = Integer Int
+  | Lambda Expr
+  | Apply Expr Expr
+  | Bound Int
+  | BinaryOp Op.BinaryOp Expr Expr
+  | SingleOp Op.SingleOp Expr
+  | Tuple [Expr]
+  | NthOf Int Expr
+  | Ref Expr
+  | Assign Expr Expr
+  | Deref Expr
+  | If Expr Expr Expr
+  deriving (Show, Eq)
+
+-- TODO: Add some optimizations:
+-- - Apply a (Bound i)
+-- - Apply a b where is_value b
+-- - bind deref'ed value in eval_thunk
+
+incr_vars :: N.Expr -> N.Expr
+incr_vars (N.Bound i)         = N.Bound $ i + 1
+incr_vars (N.Integer i)       = N.Integer i
+incr_vars (N.Lambda x)        = N.Lambda $ incr_vars x
+incr_vars (N.Apply a b)       = N.Apply (incr_vars a) (incr_vars b)
+incr_vars (N.BinaryOp op a b) = N.BinaryOp op (incr_vars a) (incr_vars b)
+incr_vars (N.SingleOp op x)   = N.SingleOp op $ incr_vars x
+incr_vars (N.Tuple xs)        = N.Tuple $ map incr_vars xs
+
+make_thunk :: N.Expr -> Expr
+make_thunk e = Ref $ Tuple [Integer 0, code]
+  where
+    code = Lambda $ NthOf 1 $ Assign (Bound 0) updated
+    updated = Tuple [Integer 1, lazy $ incr_vars e]
+
+eval_thunk :: Int -> Expr
+eval_thunk i = If cond then_ else_
+  where
+    v = Bound i
+    cond  = NthOf 0 (Deref v)
+    then_ = Apply (NthOf 1 $ Deref v) v
+    else_ = NthOf 1 $ Deref v
+
+lazy :: N.Expr -> Expr
+lazy (N.Apply a b)       = Apply (lazy a) (make_thunk b)
+lazy (N.Bound i)         = eval_thunk i
+lazy (N.Integer i)       = Integer i
+lazy (N.BinaryOp op a b) = BinaryOp op (lazy a) (lazy b)
+lazy (N.SingleOp op x)   = SingleOp op (lazy x)
+lazy (N.Tuple xs)        = Tuple $ map lazy xs
+lazy (N.Lambda body)     = Lambda $ lazy body
