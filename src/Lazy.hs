45818eb166cc9eb6ce94900aedb676b453482f87
coord.e <me@coord-e.com>
2019-06-14 17:42:01 +0000

    Change: Pass a evaluated value when the argument doesnt needed to be evaled

--- src/Lazy.hs
+++ src/Lazy.hs
@@ -19,7 +19,6 @@ data Expr
   deriving (Show, Eq)
 
 -- TODO: Add some optimizations:
--- - Apply a b where is_value b
 -- - bind deref'ed value in eval_thunk
 
 incr_vars :: Int -> N.Expr -> N.Expr
@@ -31,6 +30,9 @@ incr_vars n (N.Apply a b)             = N.Apply (incr_vars n a) (incr_vars n b)
 incr_vars n (N.BinaryOp op a b)       = N.BinaryOp op (incr_vars n a) (incr_vars n b)
 incr_vars n (N.SingleOp op x)         = N.SingleOp op $ incr_vars n x
 incr_vars n (N.Tuple xs)              = N.Tuple $ map (incr_vars n) xs
+
+make_evaled_thunk :: Expr -> Expr
+make_evaled_thunk e = Ref $ Tuple [Integer 1, e]
 
 make_thunk :: N.Expr -> Expr
 make_thunk e = Ref $ Tuple [Integer 0, code]
@@ -46,12 +48,22 @@ eval_thunk i = If cond then_ else_
     then_ = NthOf 1 $ Deref v
     else_ = Apply (NthOf 1 $ Deref v) v
 
+is_value :: N.Expr -> Bool
+is_value (N.Integer _)      = True
+is_value (N.Tuple _)        = True
+is_value (N.Lambda _)       = True
+is_value (N.Apply _ _)      = False
+is_value (N.Bound _)        = False
+is_value (N.BinaryOp _ _ _) = False
+is_value (N.SingleOp _ _)   = False
+
 lazy :: N.Expr -> Expr
-lazy (N.Apply a (N.Bound i)) = Apply (lazy a) (Bound i)
-lazy (N.Apply a b)           = Apply (lazy a) (make_thunk b)
-lazy (N.Bound i)             = eval_thunk i
-lazy (N.Integer i)           = Integer i
-lazy (N.BinaryOp op a b)     = BinaryOp op (lazy a) (lazy b)
-lazy (N.SingleOp op x)       = SingleOp op (lazy x)
-lazy (N.Tuple xs)            = Tuple $ map lazy xs
-lazy (N.Lambda body)         = Lambda $ lazy body
+lazy (N.Apply a (N.Bound i))      = Apply (lazy a) (Bound i)
+lazy (N.Apply a b)  | is_value b  = Apply (lazy a) (make_thunk b )
+                    | otherwise   = Apply (lazy a) (make_evaled_thunk $ lazy b)
+lazy (N.Bound i)                  = eval_thunk i
+lazy (N.Integer i)                = Integer i
+lazy (N.BinaryOp op a b)          = BinaryOp op (lazy a) (lazy b)
+lazy (N.SingleOp op x)            = SingleOp op (lazy x)
+lazy (N.Tuple xs)                 = Tuple $ map lazy xs
+lazy (N.Lambda body)              = Lambda $ lazy body
