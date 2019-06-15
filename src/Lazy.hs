aaf6b1fe7e7310fc9087e17fc54cbaf0fa4ba338
coord.e <me@coord-e.com>
2019-06-15 14:35:07 +0000

    Fix: use camelCase

--- src/Lazy.hs
+++ src/Lazy.hs
@@ -20,46 +20,46 @@ data Expr
   | LetBound
   deriving (Show, Eq)
 
-incr_vars :: Int -> N.Expr -> N.Expr
-incr_vars n (N.Bound i)   | i >= n    = N.Bound $ i + 1
-                          | otherwise = N.Bound i
-incr_vars n (N.Integer i)             = N.Integer i
-incr_vars n (N.Lambda x)              = N.Lambda $ incr_vars (succ n) x
-incr_vars n (N.Apply a b)             = N.Apply (incr_vars n a) (incr_vars n b)
-incr_vars n (N.BinaryOp op a b)       = N.BinaryOp op (incr_vars n a) (incr_vars n b)
-incr_vars n (N.SingleOp op x)         = N.SingleOp op $ incr_vars n x
-incr_vars n (N.Tuple xs)              = N.Tuple $ map (incr_vars n) xs
+incrVars :: Int -> N.Expr -> N.Expr
+incrVars n (N.Bound i)   | i >= n    = N.Bound $ i + 1
+                         | otherwise = N.Bound i
+incrVars n (N.Integer i)             = N.Integer i
+incrVars n (N.Lambda x)              = N.Lambda $ incrVars (succ n) x
+incrVars n (N.Apply a b)             = N.Apply (incrVars n a) (incrVars n b)
+incrVars n (N.BinaryOp op a b)       = N.BinaryOp op (incrVars n a) (incrVars n b)
+incrVars n (N.SingleOp op x)         = N.SingleOp op $ incrVars n x
+incrVars n (N.Tuple xs)              = N.Tuple $ map (incrVars n) xs
 
-make_evaled_thunk :: Expr -> Expr
-make_evaled_thunk e = Ref $ Tuple [Integer 1, e]
+makeEvaledThunk :: Expr -> Expr
+makeEvaledThunk e = Ref $ Tuple [Integer 1, e]
 
-make_thunk :: N.Expr -> Expr
-make_thunk e = Ref $ Tuple [Integer 0, code]
+makeThunk :: N.Expr -> Expr
+makeThunk e = Ref $ Tuple [Integer 0, code]
   where
     code = Lambda $ NthOf 1 $ Assign (Bound 0) updated
-    updated = Tuple [Integer 1, lazy $ incr_vars 0 e]
+    updated = Tuple [Integer 1, lazy $ incrVars 0 e]
 
-eval_thunk :: Int -> Expr
-eval_thunk i = LocalLet (Deref $ Bound i) $ If cond then_ else_
+evalThunk :: Int -> Expr
+evalThunk i = LocalLet (Deref $ Bound i) $ If cond then_ else_
   where
     cond  = NthOf 0 LetBound
     then_ = NthOf 1 LetBound
     else_ = Apply (NthOf 1 LetBound) (Bound i)
 
-is_value :: N.Expr -> Bool
-is_value (N.Integer _)      = True
-is_value (N.Tuple _)        = True
-is_value (N.Lambda _)       = True
-is_value (N.Apply _ _)      = False
-is_value (N.Bound _)        = False
-is_value (N.BinaryOp _ _ _) = False
-is_value (N.SingleOp _ _)   = False
+isValue :: N.Expr -> Bool
+isValue (N.Integer _)      = True
+isValue (N.Tuple _)        = True
+isValue (N.Lambda _)       = True
+isValue (N.Apply _ _)      = False
+isValue (N.Bound _)        = False
+isValue (N.BinaryOp _ _ _) = False
+isValue (N.SingleOp _ _)   = False
 
 lazy :: N.Expr -> Expr
 lazy (N.Apply a (N.Bound i))      = Apply (lazy a) (Bound i)
-lazy (N.Apply a b)  | is_value b  = Apply (lazy a) (make_thunk b )
-                    | otherwise   = Apply (lazy a) (make_evaled_thunk $ lazy b)
-lazy (N.Bound i)                  = eval_thunk i
+lazy (N.Apply a b)  | isValue b   = Apply (lazy a) (makeThunk b )
+                    | otherwise   = Apply (lazy a) (makeEvaledThunk $ lazy b)
+lazy (N.Bound i)                  = evalThunk i
 lazy (N.Integer i)                = Integer i
 lazy (N.BinaryOp op a b)          = BinaryOp op (lazy a) (lazy b)
 lazy (N.SingleOp op x)            = SingleOp op (lazy x)
