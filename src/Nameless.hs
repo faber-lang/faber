a109af0c27da1468c45b1236718eecd02e0afe2f
coord.e <me@coord-e.com>
2019-06-18 00:17:51 +0000

    Add: Complete the implementation of nameless conversion on defs

--- src/Nameless.hs
+++ src/Nameless.hs
@@ -16,11 +16,21 @@ data Expr
   | Tuple [Expr]
   deriving (Show, Eq)
 
+data DefBody
+  = Name Expr
+
+data Def = Def String DefBody
+
+type Code = [Def]
+
+-- types for the conversion
 data Binding
   = Global String
   | Param String
 
 type Env = [Binding]
+initEnv :: Env
+initEnv = []
 withBinding :: Binding -> Reader Env m -> Reader Env m
 withBinding new = local (new:)
 
@@ -38,14 +48,23 @@ findName s = do
   env <- ask
   return $ runReader (findInEnv env s) 0
 
-nameless' :: D.Expr -> Nameless Expr
-nameless' (D.Apply fn arg) = Apply <$> nameless' fn <*> nameless' arg
-nameless' (D.Lambda p body) = Lambda <$> withBinding (Param p) (nameless' body)
-nameless' (D.Variable name) = findName name
-nameless' (D.Integer i) = return $ Integer i
-nameless' (D.BinaryOp op a b) = BinaryOp op <$> nameless' a <*> nameless' b
-nameless' (D.SingleOp op x) = SingleOp op <$> nameless' x
-nameless' (D.Tuple xs) = Tuple <$> mapM nameless' xs
+namelessExpr :: D.Expr -> Nameless Expr
+namelessExpr (D.Apply fn arg) = Apply <$> namelessExpr fn <*> namelessExpr arg
+namelessExpr (D.Lambda p body) = Lambda <$> withBinding (Param p) (namelessExpr body)
+namelessExpr (D.Variable name) = findName name
+namelessExpr (D.Integer i) = return $ Integer i
+namelessExpr (D.BinaryOp op a b) = BinaryOp op <$> namelessExpr a <*> namelessExpr b
+namelessExpr (D.SingleOp op x) = SingleOp op <$> namelessExpr x
+namelessExpr (D.Tuple xs) = Tuple <$> mapM namelessExpr xs
 
-nameless :: D.Expr -> Expr
-nameless e = runReader (nameless' e) []
+namelessDefBody :: D.DefBody -> Nameless DefBody
+namelessDefBody (D.Name body) = Name <$> namelessExpr body
+
+namelessDef :: D.Def -> Nameless Def
+namelessDef (D.Def name body) = Def name <$> withBinding (Global name) (namelessDefBody body)
+
+namelessCode :: D.Code -> Nameless Code
+namelessCode = mapM namelessDef
+
+nameless :: D.Code -> Code
+nameless c = runReader (namelessCode c) initEnv
