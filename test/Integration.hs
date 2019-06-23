251f018889a21d09a604bb99eec90cfb4a4f4f03
coord.e <me@coord-e.com>
2019-06-23 06:22:36 +0000

    Add: Add some simple examples

--- test/Integration.hs
+++ test/Integration.hs
@@ -10,10 +10,8 @@ import           Test.Hspec
 
 import Compile
 
-execFile :: String -> IO BS.ByteString
-execFile filename = do
-  content <- readFile filename
-  execStr content
+execExpr :: String -> IO BS.ByteString
+execExpr expr = execStr $ "name main = " ++ expr
 
 execStr :: String -> IO BS.ByteString
 execStr content = do
@@ -22,11 +20,13 @@ execStr content = do
       withSystemTempFile "fabertest.ll" $ \path handle -> do
         BS.hPut handle =<< BS.fromStrict <$> moduleToLLVMAssembly m
         hClose handle
-        fst <$> (readProcess_ $ shell $ "lli " ++ path)
+        fst <$> readProcess_ (shell $ "lli " ++ path)
     Left err -> error $ show err
 
 spec :: Spec
 spec = do
-  describe "simple" $ do
-    it "int" $ do
-      execStr "name main = 10" `shouldReturn` "10\n"
+  describe "Integration Tests" $ do
+    it "return int" $ execExpr "10" `shouldReturn` "10\n"
+    it "function application" $ execExpr "(\\x=>x) 42" `shouldReturn` "42\n"
+    it "passing function" $ execExpr "(\\f x => f (f x)) (\\x => x+1) 3" `shouldReturn` "5\n"
+    it "many arguments" $ execExpr "(\\a b c d => a + b + c + d) 1 2 3 4" `shouldReturn` "10\n"
